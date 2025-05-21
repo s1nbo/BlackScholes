626fea63ca5dc0f98846dce5803afa7aae138294
s1nbo <77055060+s1nbo@users.noreply.github.com>
2025-05-21 17:09:43 +0200

    fixed typos

--- ui.py
+++ ui.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from blackSholesBasics import blackScholes
+from blackScholesBasics import blackScholes
 from css import css
 
 linkedin_url = 'https://www.linkedin.com/in/sinbo/'
@@ -7,7 +7,7 @@ linkedin_url = 'https://www.linkedin.com/in/sinbo/'
 css()
 
 with st.sidebar:
-    st.title('Black-Sholes Option Pricer')
+    st.title('Black-Scholes Option Model')
     st.markdown(f'<a href="{linkedin_url}" target="_blank" style="text-decoration: none; color: inherit;"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width="25" height="25" style="vertical-align: middle; margin-right: 10px;">Sinan Bove</a>', unsafe_allow_html=True)
 
     current_price = st.number_input("Current Asset Price", value=100.0, step=1.0, min_value=0.01)
