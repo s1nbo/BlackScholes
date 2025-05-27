a469d532cf2c46d1852af6bf89f1a4a4815ffc5a
s1nbo <77055060+s1nbo@users.noreply.github.com>
2025-05-27 22:45:54 +0200

    Minor changes to finish this mini project

--- ui.py
+++ ui.py
@@ -20,10 +20,13 @@ with st.sidebar:
     volatility = st.number_input("Volatility (σ)", value=0.2, min_value=0.0)
     interest_rate = st.number_input("Risk-Free Interest Rate", value=0.05)
 
-
-    C, P = blackScholes(current_price=current_price, strike_price=strike, time_to_maturity=time_to_maturity, volatility=volatility, interest_rate=interest_rate)
-    st.markdown(f'Call Price: {C}')
-    st.markdown(f'Put Price: {P}')
+    C, P = blackScholes(
+        current_price=current_price, 
+        strike_price=strike, 
+        time_to_maturity=time_to_maturity, 
+        volatility=volatility, 
+        interest_rate=interest_rate
+    )
 
 col1, col2 = st.columns([1,1], gap="small")
 
@@ -57,7 +60,13 @@ col1, col2 = st.columns([1,1], gap="small")
 spot_range = linspace(current_price - 20, current_price + 20, 10)
 vol_range = linspace(volatility - 0.1, volatility + 0.1, 10)
 
-heatmap_fig_call, heatmap_fig_put = plot_heatmap(strike=strike, time_to_maturity=time_to_maturity, interest_rate=interest_rate, spot_range=spot_range, vol_range=vol_range)
+heatmap_fig_call, heatmap_fig_put = plot_heatmap(
+    strike=strike, 
+    time_to_maturity=time_to_maturity, 
+    interest_rate=interest_rate, 
+    spot_range=spot_range, 
+    vol_range=vol_range
+)
 
 with col1:
     st.subheader("Call Price Heatmap")
@@ -66,44 +75,3 @@ with col1:
 with col2:
     st.subheader("Put Price Heatmap")
     st.pyplot(heatmap_fig_put)
-
-
-st.title("Stock Plotting")
-
-data = stockdata()
-# Get axis limits
-x_min, x_max = data.index.min(), data.index.max()
-y_min, y_max = data.min()[0], data.max()[0]
-
-# Create a placeholder
-plot_placeholder = st.empty()
-
-# Create figure and axis
-fig, ax = plt.subplots()
-
-# Set static axes limits
-ax.set_xlim(x_min, x_max)
-ax.set_ylim(y_min, y_max)
-
-# Animation loop
-for i in range(1, len(data) + 1, 10):
-    ax.clear()
-
-    # Redraw line with static axis limits
-    ax.plot(data.index[:i], data.values[:i], color='blue')
-    ax.set_title("AAPL Closing Prices (2020)")
-    ax.set_xlabel("Date")
-    ax.set_ylabel("Price (USD)")
-    ax.grid(True)
-    
-    # Static limits
-    ax.set_xlim(x_min, x_max)
-    ax.set_ylim(y_min, y_max)
-
-    # X Axis Labels
-    ax.xaxis.set_major_locator(mdates.MonthLocator())
-    ax.xaxis.set_major_formatter(mdates.DateFormatter('%b %Y'))
-    plt.setp(ax.get_xticklabels(), rotation=45, ha='right')
-
-
-    plot_placeholder.pyplot(fig)
